bb6dd2288b5be0dc6dfa99a0131dc1f45d7a3c91
TejasviHari <tejasvihari21@gmail.com>
2020-12-29 09:56:07 +0530

    end game functioned

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
+pair=localStorage.getItem("pair");
+user=localStorage.getItem("user");
+friend=localStorage.getItem("friend");
+document.getElementById("disclaimer").innerHTML="Nice Game"+pair+"!!";
+document.getElementById("description").innerHTML=user+" and "+friend;
+
+
 function logout(){
     window.location="index.html"
     
