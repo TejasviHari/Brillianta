97572563fd6de1bdac142fb75623265b73958bb8
TejasviHari <tejasvihari21@gmail.com>
2021-01-03 12:26:14 +0530

    folder division divided update

---
+++ application/intro.js
@@ -0,0 +1,16 @@
+user=localStorage.getItem("user");
+
+
+document.getElementById("user_label").innerHTML="User name: "+user;
+
+function logout(){
+    window.location="index.html"
+    
+}
+
+
+function english(){
+    chosen_course="English"
+    localStorage.setItem("course",chosen_course);
+    window.location="english.html"
+}
