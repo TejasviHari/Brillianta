da75488870dc46093c1498e08da68f5bf49695e3
TejasviHari <tejasvihari21@gmail.com>
2021-08-20 10:42:15 +0530

    not working but updating

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
  
 
 function login(){
- pair=document.getElementById("pair_name").value;
- friend=document.getElementById("friend_name").value;
- user_name=document.getElementById("name").value;
-localStorage.setItem("pair",pair);
-localStorage.setItem("friend",friend);
-localStorage.setItem("user",user_name);
 window.location="intro.html";
 }
